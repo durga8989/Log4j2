a73ac2eb94e718d5dfb5e89c12e6ad7a20261b50
durga <durga.nayak@profinch.com>
2022-10-03 16:12:55 +0530

    resolved conflicts

--- log4j/src/alpha/Demo.java
+++ log4j/src/alpha/Demo.java
@@ -7,9 +7,7 @@ public class Demo {
 	private static Logger log = LogManager.getLogger(Demo.class.getName());
 	public static void main(String[] args) {
 		log.error("log is success");
-		System.out.println("checking git");
-		System.out.println("added 2nd line");
-		System.out.println("added 3rd line");
+
 	}
 
 }
