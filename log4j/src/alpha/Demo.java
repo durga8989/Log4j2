9f42c2882c30ad818a8c0c1f764b8bd8e8919c0f
durga <durga.nayak@profinch.com>
2022-10-03 16:19:19 +0530

    added

--- log4j/src/alpha/Demo.java
+++ log4j/src/alpha/Demo.java
@@ -7,9 +7,12 @@ public class Demo {
 	private static Logger log = LogManager.getLogger(Demo.class.getName());
 	public static void main(String[] args) {
 		log.error("log is success");
+<<<<<<< HEAD
 		System.out.println("adding 1 line");
 		System.out.println("adding 2 line");
 		System.out.println("adding 3 line");
+=======
+>>>>>>> parent of 529e193 (1st commit)
 
 	}
 
