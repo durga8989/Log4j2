b95e9b984a8ae2ba9cbb2717e86b2591047897e1
durga <durga.nayak@profinch.com>
2022-10-03 17:57:35 +0530

    add

--- log4j/src/alpha/NewFile.java
+++ log4j/src/alpha/NewFile.java
@@ -4,10 +4,7 @@ public class NewFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("line1");
-		System.out.println("line2");
-		System.out.println("line3");
-		System.out.println("line4");
+		
 
 	}
 
