54ab6106a3f6d77544cc6b7ed18b235fd8a008f8
durga <durga.nayak@profinch.com>
2022-10-03 15:20:14 +0530

    added 2nd line

--- log4j/src/alpha/Demo.java
+++ log4j/src/alpha/Demo.java
@@ -8,6 +8,7 @@ public class Demo {
 	public static void main(String[] args) {
 		log.error("log is success");
 		System.out.println("checking git");
+		System.out.println("added 2nd line");
 	}
 
 }
