48e84d965c99ed80ad14a13bf635ea84369b8b1f
durga <durga.nayak@profinch.com>
2022-10-04 17:27:47 +0530

    rev

--- log4j/src/alpha/NewFile.java
+++ log4j/src/alpha/NewFile.java
@@ -6,13 +6,7 @@ public class NewFile {
 		// TODO Auto-generated method stub
 		int a;
 		int b;
-<<<<<<< HEAD
 
-=======
-		System.out.println("checking git revert");
-		System.out.println("checking git revert");
-		
->>>>>>> parent of cd1882b (added 3rd print line)
 
 		
 
