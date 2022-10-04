d2a09a77344afbb815bf951135596a27a776db06
durga <durga.nayak@profinch.com>
2022-10-04 17:03:52 +0530

    added print line

--- log4j/src/alpha/NewFile.java
+++ log4j/src/alpha/NewFile.java
@@ -1,11 +1,12 @@
 package alpha;
-
+//new
 public class NewFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a;
 		int b;
+		System.out.println("checking git revert");
 
 		
 
